9e67cf783eafb695b0c8d528640f35b7f38bc402
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-11-26 00:51:34 -0800

    wip

--- RyanScraper/RyanScraper/spiders/ryan_spider.py
+++ RyanScraper/RyanScraper/spiders/ryan_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 import logging
+
 from bs4 import BeautifulSoup
+from datetime import datetime
 from scrapy.spiders import CrawlSpider
 from .helper import *
 
@@ -13,7 +15,7 @@ class RyanSpider(CrawlSpider):
         self.teams = teams.split(',')
 
         # Earliest year for which we want data
-        self.earliest_year = earliest_year
+        self.earliest_year = int(earliest_year)
 
         self.base_url = 'https://www.pro-football-reference.com'
 
@@ -27,7 +29,13 @@ class RyanSpider(CrawlSpider):
         # Get links for teams we're interested in
         team_regex = re.compile(build_regex_or(self.teams))
         for a in soup.find_all('a', text=team_regex):
-            yield scrapy.Request(url=base_url + a['href'], callback=self.get_team_data)
+            yield scrapy.Request(url=self.base_url + a['href'], callback=self.get_team_data)
 
     def get_team_data(self, response):
         soup = BeautifulSoup(response.body, 'lxml')
+
+        years = [str(self.earliest_year + y) for y in
+            range(datetime.now().year - self.earliest_year + 1)]
+        year_regex = re.compile(build_regex_or(years))
+        for a in soup.find_all('a', text=year_regex):
+            print(a.parent.parent)
