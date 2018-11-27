39ae09da9fe80b3e49f44cc20fdf9383082e9a7d
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-11-26 22:28:41 -0800

    done!

--- RyanScraper/RyanScraper/spiders/helper.py
+++ RyanScraper/RyanScraper/spiders/helper.py
@@ -1,9 +1,12 @@
 import re
+import csv
 
 from bs4 import BeautifulSoup
 
 def build_regex_or(strings, file_extension=False):
-    '''Return a regex that matches any strings in a given list'''
+    '''
+    Return a regex that matches any strings in a given list
+    '''
     regex = ''
     letter_re = re.compile('[A-Z]|[a-z]')
     for i, string in enumerate(strings):
@@ -21,11 +24,25 @@ def build_regex_or(strings, file_extension=False):
     return regex
 
 def get_data_stat(dom_obj, data_stat):
+    '''
+    Return a BS4 dom object for the specified "data-stat" value.
+    '''
     for d in dom_obj.find_all('td', attrs={'data-stat': data_stat}):
         return d
 
 def add_to_row(data_dict, team, field, value):
-    if team not in data_dict:
-        data_dict[team] = {}
-
+    '''
+    Add to a team's data row.
+    '''
     data_dict[team][field] = value
+
+def write_to_csv(data_dict, csv_file_path, csv_header_fields):
+    '''
+    Write data from dictionary to CSV.
+    '''
+    with open(csv_file_path, 'w') as f:
+        w = csv.DictWriter(f, fieldnames=csv_header_fields)
+        w.writeheader()
+
+        for team in data_dict:
+            w.writerow(data_dict[team])
--- RyanScraper/RyanScraper/spiders/ryan_spider.py
+++ RyanScraper/RyanScraper/spiders/ryan_spider.py
@@ -11,37 +11,112 @@ class RyanSpider(CrawlSpider):
     # Ryan is an Internet arachnid
     name = 'Ryan'
 
-    def __init__(self, teams='', year=2017):
+    def __init__(self, teams='', year=2017, csv_file_path=''):
+        '''
+        Build-a-Ryan.
+        '''
         # Teams to search for (passed with -a option on cmd line)
-        self.teams = teams.split(',')
+        if teams != '':
+            self.teams = teams.split(',')
+        else:
+            self.teams = [
+                'Arizona Cardinals',
+                'Atlanta Falcons',
+                'Baltimore Ravens',
+                'Buffalo Bills',
+                'Carolina Panthers',
+                'Chicago Bears',
+                'Cincinnati Bengals',
+                'Cleveland Browns',
+                'Dallas Cowboys',
+                'Denver Broncos',
+                'Detroit Lions',
+                'Green Bay Packers',
+                'Houston Texans',
+                'Indianapolis Colts',
+                'Jacksonville Jaguars',
+                'Kansas City Chiefs',
+                'Miami Dolphins',
+                'Minnesota Vikings',
+                'New England Patriots',
+                'New Orleans Saints',
+                'New York Giants',
+                'New York Jets',
+                'Oakland Raiders',
+                'Philadelphia Eagles',
+                'Pittsburgh Steelers',
+                'Los Angeles Rams',
+                'Los Angeles Chargers',
+                'San Francisco 49ers',
+                'Seattle Seahawks',
+                'Tampa Bay Buccaneers',
+                'Tennessee Titans',
+                'Washington Redskins'
+            ]
 
         # Year we're interested in
         self.year = str(year)
 
+        self.csv_file_path = csv_file_path
+        if csv_file_path == '':
+            self.csv_file_path = self.year + '.csv'
+
+        # Regex to match all teams
+        self.team_regex = re.compile(build_regex_or(self.teams))
+
         # Regex to find team pages for our year
         self.year_regex = re.compile('teams\/[a-z]{3}\/' + self.year + '.htm')
 
+        # Store data in dictionary before it's written to a file
+        self.data_dict = {}
+        for team in self.teams:
+            self.data_dict[team] = {}
+            self.data_dict[team]['team'] = team
+
+
+        # Fields on team-specific page
         self.team_page_fields = [
             'points',
             'points_opp',
             'points_diff'
         ]
 
+        # Fields on opposition and defensive page
+        self.opp_def_page_fields = [
+            'points',
+            'total_yards',
+            'plays_offense',
+            'fumbles_lost',
+            'pass_att',
+            'pass_cmp',
+            'turnover_pct',
+            'exp_pts_def_tot',
+            'penalties_yds'
+        ]
+
+        self.csv_header_fields = ['team'] + self.team_page_fields + \
+            self.opp_def_page_fields
+
         # Landing page
         self.base_url = 'https://www.pro-football-reference.com'
-
-        # Store data in dictionary before it's written to a file
-        self.data = {}
 
         # Write data on spider close
         dispatcher.connect(self.write_data, signals.spider_closed)
 
     def write_data(self, spider):
-        print(self.data)
+        '''
+        Write data from our dictionary to a CSV file.
+        '''
+        write_to_csv(self.data_dict, self.csv_file_path, self.csv_header_fields)
 
     def start_requests(self):
+        '''
+        Start requests off by visiting generic teams page and the opposition and defensive
+        stats page for the year we care about.
+        '''
+        yield scrapy.Request(url=self.base_url + '/years/{}/opp.htm'.format(self.year),
+            callback=self.parse_opp_def_page)
         yield scrapy.Request(url=self.base_url + '/teams', callback=self.get_team_pages)
-        yield scrapy.Request(url=self.base_url + '/years/{}/opp.htm'.format(self.year), callback=self.parse_opp_def_page)
 
     def get_team_pages(self, response):
         '''
@@ -50,8 +125,7 @@ class RyanSpider(CrawlSpider):
         soup = BeautifulSoup(response.body, 'lxml')
 
         # Get links to team pages
-        team_regex = re.compile(build_regex_or(self.teams))
-        for a in soup.find_all('a', text=team_regex):
+        for a in soup.find_all('a', text=self.team_regex):
             # Team's base page has some basic stats not available from
             # more detailed view
             team_url = self.base_url + a['href']
@@ -59,6 +133,9 @@ class RyanSpider(CrawlSpider):
                 meta={'team': a.text})
 
     def parse_team_page(self, response):
+        '''
+        Get data from a team's page.
+        '''
         soup = BeautifulSoup(response.body, 'lxml')
 
         team = response.meta.get('team')
@@ -68,7 +145,22 @@ class RyanSpider(CrawlSpider):
 
             for field in self.team_page_fields:
                 value = get_data_stat(tr, field).text
-                add_to_row(self.data, team, field, value)
+                add_to_row(self.data_dict, team, field, value)
 
     def parse_opp_def_page(self, response):
+        '''
+        Get data from the opposition and defensive stats page.
+        '''
         soup = BeautifulSoup(response.body, 'lxml')
+
+        for a in soup.find_all('a', text=self.team_regex):
+            team = a.text
+            tr = a.parent.parent
+
+            for field in self.opp_def_page_fields:
+                try:
+                    value = get_data_stat(tr, field).text
+                    add_to_row(self.data_dict, team, field, value)
+                except:
+                    logging.warn('get_data_stat() failed for team = {} and field = {}'.format(
+                        team, field))
